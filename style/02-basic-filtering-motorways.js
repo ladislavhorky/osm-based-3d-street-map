c2959ec87fde1408e155ad11fecf46a72c060235
Ladislav Horký <ladislavhorky@users.noreply.github.com>
2019-04-16 01:09:18 +0200

    fixed 02 style

--- style/02-basic-filtering-motorways.js
+++ style/02-basic-filtering-motorways.js
@@ -9,7 +9,8 @@ $02BasicFiltering = {
         "filter" : ["all",["==","#group","transportation"],["in","$class","motorway"]],
         "line" : true,
         "line-width" : 4,
-        "line-color" : [255,235,160,128]
+        "line-color" : [255,235,160,128],
+        "zbuffer-offset": [-0.02,0,0]
     }
 }
 }
